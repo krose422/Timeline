e847da7fbf77ff20c851384aad69402fa8a60d40
Kelley Rose <krose422@gmail.com>
2015-06-02 19:25:43 -0400

    Cleaned up code, worked on centering icons more

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done(functi
     data.timelines.forEach(function(timeline) {
       if(timeline.timestamp) {
         timeline.date = moment(new Date(timeline.timestamp)).format('M/D/YY');
-        timeline.times = moment(new Date(timeline.timestamp)).format('hh:mm');
+        timeline.times = moment(new Date(timeline.timestamp)).format('HH:mm');
       }
       console.log(timeline);
       return timeline;
--- js/templates.js
+++ js/templates.js
@@ -8,7 +8,7 @@ this["template"]["example"] = Handlebars.template({"1":function(depth0,helpers,p
     + alias3(((helper = (helper = helpers.times || (depth0 != null ? depth0.times : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"times","hash":{},"data":data}) : helper)))
     + "</p>\n    </div>\n\n    <div class=\"circleicon\">\n      <div class=\"circletwo\">\n        <div class=\"circleone\">\n          <i class=\"fa "
     + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
-    + "\"></i>\n        </div>\n      </div>\n    </div>\n\n  </div> <!-- end top div -->\n\n\n  <div class=\"maincontent\">\n    <div class=\"title\">\n      <div class=\"triangle\"></div>\n      <h3>"
+    + "\"></i>\n        </div>\n      </div>\n    </div>\n\n  </div> <!-- end top div -->\n\n\n  <div class=\"maincontent\">\n\n    <div class=\"title\">\n      <div class=\"triangle\"></div>\n      <h3>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h3>\n    </div>\n\n    <div class=\"content\">\n      <p>"
     + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
