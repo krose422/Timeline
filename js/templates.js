b8ef0542f1dc606b332ab2bc86268cd5200de0fc
Kelley Rose <krose422@gmail.com>
2015-06-02 19:13:16 -0400

    Cleaned up code

--- js/templates.js
+++ js/templates.js
@@ -2,21 +2,21 @@ this["template"] = this["template"] || {};
 this["template"]["example"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "\n<div class=\"main\">\n<div class=\"top\">\n\n  <div class=\"timestamp\">\n    <p class=\"date\">"
+  return "\n<div class=\"main\">\n\n  <div class=\"top\">\n\n    <div class=\"timestamp\">\n      <p class=\"date\">"
     + alias3(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"date","hash":{},"data":data}) : helper)))
-    + "</p>\n    <p class=\"time\">"
+    + "</p>\n      <p class=\"time\">"
     + alias3(((helper = (helper = helpers.times || (depth0 != null ? depth0.times : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"times","hash":{},"data":data}) : helper)))
-    + "</p>\n  </div>\n\n  <div class=\"circleicon\">\n  <div class=\"circletwo\">\n    <div class=\"circleone\">\n      <i class=\"fa "
+    + "</p>\n    </div>\n\n    <div class=\"circleicon\">\n      <div class=\"circletwo\">\n        <div class=\"circleone\">\n          <i class=\"fa "
     + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
-    + "\"></i>\n    </div>\n  </div>\n  </div>\n\n</div>\n\n\n<div class=\"maincontent\">\n  <div class=\"title\">\n  <div class=\"triangle\"></div>\n    <h3>"
+    + "\"></i>\n        </div>\n      </div>\n    </div>\n\n  </div> <!-- end top div -->\n\n\n  <div class=\"maincontent\">\n    <div class=\"title\">\n      <div class=\"triangle\"></div>\n      <h3>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h3>\n  </div>\n\n  <div class=\"content\">\n    <p>"
+    + "</h3>\n    </div>\n\n    <div class=\"content\">\n      <p>"
     + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
-    + "</p>\n  </div>\n\n</div>\n\n</div>\n\n";
+    + "</p>\n    </div>\n\n  </div> <!-- end maincontent div -->\n\n</div> <!-- end main div -->\n\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
   return "\n<div class=\"all\">\n\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.timelines : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "\n</div>\n\n\n";
+    + "\n</div> <!-- end all div -->\n\n\n";
 },"useData":true});
